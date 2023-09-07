688ec61e9c66105864bafa16baa9b479882a8665
mitchelle lindsey <mitchellelindsey@gmail.com>
2023-09-07 12:10:18 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is a function in c
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("the size of a char: %c", size of a char));
+	printf("the size of an int: %d", size of an int));
+	printf("the size of a long int: %d", size of a long int));
+	printf("the size of a long long int: %d", size of a long long int));
+	printf("the size of a float: %f", size of a float));
+	return (0);
+}
