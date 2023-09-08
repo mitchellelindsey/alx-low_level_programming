fe605cece204101cad59c936d2bbbe7c009bbc24
mitchelle lindsey <mitchellelindsey@gmail.com>
2023-09-08 11:29:51 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	char j
+
+		for (j = 'z' ; j >= 'a' ; j--)
+			putchar(j);
+	putchar('\n');
+
+	return (0);
+
+}
