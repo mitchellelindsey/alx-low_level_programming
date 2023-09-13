e6a6b539685bd05b616dbbc871f41048a799d88d
mitchelle lindsey <mitchellelindsey@gmail.com>
2023-09-12 17:25:01 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,24 +9,14 @@
 
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2;
+	int sum = 0;
+	int i;
 
+	for (i - 1 ; i <= 10 ; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+	}
+	printf("%d\n", sum);
 
-	while (i < 50)
-	{
-	if (i == 0)
-	printf("%ld", j);
-	else if (i == 1)
-	printf(", %ld", k);
-	else
-	{
-	k += j;
-	j = k - j;
-	printf(", %ld", k);
-	}
-	++i;
-	}
-	printf("\n");
-	return (0);
 }
