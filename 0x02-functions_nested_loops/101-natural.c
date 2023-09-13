262a75d88170bc837805f64c212f7c71b964f152
mitchelle lindsey <mitchellelindsey@gmail.com>
2023-09-12 17:46:22 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,21 +1,22 @@
-#include <stdio.h>
+#include "main.h"
+
 
 /**
- * main - main function
+ * main - check the code.
  *
- * Return: 0
+ * Return: Always 0.
  */
 
+
 int main(void)
-
 {
-	int sum = 0;
-	int i;
 
-	for (i = 1 ; i < 1024 ; i++)
-	{
-		if (i % 3 == 0 || i % 5 == 0)
-			sum = sum + i;
-	}
-	printf("%d\n", sum);
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	return (0);
 }
