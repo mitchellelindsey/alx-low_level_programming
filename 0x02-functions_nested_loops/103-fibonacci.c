fdbee32a77040d7295198c731ea2621478cce3bb
mitchelle lindsey <mitchellelindsey@gmail.com>
2023-09-12 18:58:04 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,17 +8,20 @@
 
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2, sum = k;
+	int limit = 4000000
+	int term1 = 1;
+	int term2 = 2;
+	int next;
+	int sum = 2
 
-	while (k + j < 4000000)
+	while (next <= limit)
 	{
-		k += j;
-		if (k % 2 == 0)
-		sum += k;
-		j = k - j;
-		++i;
+		next = term1 + term2;
+		if (next % 2 == 0)
+			sum = sum + 2;
+		term1 = term2;
+		term2 = next;
 	}
-	printf("%ld", sum);
+	printf("%d\n", sum);
 	return (0);
 }
