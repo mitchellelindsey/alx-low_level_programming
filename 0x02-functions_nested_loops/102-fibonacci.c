e462f52a75673c551d71782b3ac65a9adb6ebece
mitchelle lindsey <mitchellelindsey@gmail.com>
2023-09-12 18:42:45 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
-
 /**
-  * main - prints the first 52 fib
-  * Return: 0.
+  * main - prints the first 52 fibonacci numbers
+  * Return: 0
   */
-
 
 int main(void)
 {
-	unsigned long int term1 = 1;
-	unsigned long int term2 = 2;
-	unsigned long int next;
-	int i;
+	int i = 0;
+	long j = 1, k = 2;
 
-	printf("%lu, ", term1);
-	for (i = 1 ; i < 50 ; i++)
+	while (i < 50)
 	{
-		printf("%lu", term2);
-		next = term1 + term2;
-		term1 = term2;
-		term2 = next;
-		if (i != 49)
-			printf(", ");
+		if (i == 0)
+			printf("%ld", j);
+		else if (i == 1)
+			printf(", %ld", k);
+		else
+		{
+			k += j;
+			j = k - j;
+			printf(", %ld", k);
+		}
+		++i;
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
