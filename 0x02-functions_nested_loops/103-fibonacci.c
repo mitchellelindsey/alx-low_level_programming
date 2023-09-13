92a2f8036e55f2398a07782f46a93137c160998e
mitchelle lindsey <mitchellelindsey@gmail.com>
2023-09-12 18:46:42 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 
-
 /**
   * main - the main printing funtion
   * less than 4000000.
   * Return: 0.
   */
 
-
 int main(void)
 {
 	int i = 0;
 	long j = 1, k = 2, sum = k;
-
 
 	while (k + j < 4000000)
 	{
