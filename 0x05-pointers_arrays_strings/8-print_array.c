b858726324168bcb8f0bea2e260e14261d905bf5
mitchelle lindsey <mitchellelindsey@gmail.com>
2023-09-18 18:09:10 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,16 +7,15 @@
  */
 
 void print_array(int *a, int n)
-{
+{i
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0 ; i < n ; i++)
 	{
-	printf("%d", a[i]);
 
+		printf("%d", a[i]);
 		if (i < n - 1)
 			printf(", ");
-
-	printf("\n");
 	}
+	printf("\n");
 }
