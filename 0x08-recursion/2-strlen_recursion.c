e67c0ed21afc97504d690c08b64931b4d6611d52
mitchelle lindsey <mitchellelindsey@gmail.com>
2023-09-28 13:40:19 -0700

    2-strlen_recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: function parameter
- * Return: length of string as an int 
+ * Return: length of string as an int
  */
 
 int _strlen_recursion(char *s)
