e37820948af894bc2be8a3522df718bc007ff4fd
mitchelle lindsey <mitchellelindsey@gmail.com>
2023-09-28 15:39:30 -0700

    palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,6 +13,7 @@ int wildcmp(char *s1, char *s2);
 int sqrt_a(int a, int b);
 int prime_a(int a, int b);
 int is_palindrome(char *s);
-
+int _strlen_recursion(char *s);
+int compare_string(char *s, int left, int right);
 
 #endif
