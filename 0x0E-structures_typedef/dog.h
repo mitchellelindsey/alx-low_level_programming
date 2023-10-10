07f59107220df18d73984f387996dd71640591aa
mitchelle lindsey <mitchellelindsey@gmail.com>
2023-10-10 13:19:01 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - a struct that creates a dog info
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Desc: first struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
