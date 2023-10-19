5a83d03013f998504b39f895806af5156393d26e
mitchelle lindsey <mitchellelindsey@gmail.com>
2023-10-19 05:40:37 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * comefirst - function that runs before main function
+ */
+
+void comefirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
