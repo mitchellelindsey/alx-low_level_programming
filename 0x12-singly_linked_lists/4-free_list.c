bd4fc22008ba37d0fc2744736853630df162af05
mitchelle lindsey <mitchellelindsey@gmail.com>
2023-10-19 05:33:53 -0700

    freelist

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: parameter
+ * Return:
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (head == NULL)
+		return;
+	while (head->next != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+	free(head->str);
+	free(head);
+}
