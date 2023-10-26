4002bdf2aecb61974c364b479cc0176f7c41ed06
mitchelle lindsey <mitchellelindsey@gmail.com>
2023-10-26 09:03:58 -0700

    printbinary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -11,5 +12,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+void print_bi(unsigned long int n);
 
 #endif
